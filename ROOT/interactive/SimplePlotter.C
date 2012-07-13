66d866fae88f2c84fae533f3076006a79a09ee58
Austin Belknap <dabelknap@wisc.edu>
2012-07-13 08:13:36 -0500

    Changed font sizes for comparison plot labels

--- ROOT/interactive/SimplePlotter.C
+++ ROOT/interactive/SimplePlotter.C
@@ -610,48 +610,52 @@ public:
 
   TCanvas* makeComparison(TString var,TString selection,TString lumi, int bins, float min, float max,TString labelX)
   {
-    THStack *hs = new THStack("hs","");
-    TLegend *l = new TLegend(0.6,0.6,0.9,0.9);
-    
+      THStack *hs = new THStack("hs","");
+      TLegend *l = new TLegend(0.6,0.6,0.9,0.9);
 
-     std::vector<TH1F*> histos;
-    for(unsigned int i=0;i<trees_.size();++i)
+
+      std::vector<TH1F*> histos;
+      for(unsigned int i=0;i<trees_.size();++i)
       {
-	TH1F * hh = new TH1F("hh","hh",bins,min,max);
-	hh->Sumw2();
-	trees_[i]->Draw(var +">>hh",lumi+"*"+preselection_[i]+"*("+selection+")","goff");
-	if(lumi=="1")  
-	  hh->Scale((float)1./hh->Integral());
+          TH1F * hh = new TH1F("hh","hh",bins,min,max);
+          hh->Sumw2();
+          //trees_[i]->Draw(var +">>hh",lumi+"*"+preselection_[i]+"*("+selection+")","goff");
+          trees_[i]->Draw(var +">>hh",preselection_[i]+"*("+selection+")","goff");
+          if(lumi=="1")  
+              hh->Scale((float)1./hh->Integral());
 
-	if(lumi=="0")  
-	  hh->Scale((float)1./hh->GetEntries());
+          if(lumi=="0")  
+              hh->Scale((float)1./hh->GetEntries());
 
 
-	printf(" %s = %f\n",labels_[i].Data(),hh->Integral());
-	
-	hh->SetLineWidth(2);
-	hh->SetLineColor(colors_[i]);
-	hh->SetName("hh"+var);
-	hh->GetXaxis()->SetTitle(labelX);
-	hh->GetYaxis()->SetTitle("a.u");
-	l->AddEntry(hh,labels_[i],"l");	
-	histos.push_back(hh);
-	
+          printf(" %s = %f\n",labels_[i].Data(),hh->Integral());
+
+          hh->SetLineWidth(2);
+          hh->SetLineColor(colors_[i]);
+          hh->SetName("hh"+var);
+          hh->GetXaxis()->SetTitle(labelX);
+          hh->GetYaxis()->SetTitle("a.u");
+          l->AddEntry(hh,labels_[i],"l");	
+          histos.push_back(hh);
+
       }
-    for(unsigned int i=0;i<histos.size();++i)
-	hs->Add(histos[i]);
-   
-    l->SetFillColor(kWhite);
-    l->SetBorderSize(0);
-    
-    TCanvas * c = new TCanvas(var,var,600,600);
-    c->cd();
-    hs->Draw("HIST,NOSTACK");
-    hs->GetXaxis()->SetTitle(labelX);
-    l->Draw();
+      for(unsigned int i=0;i<histos.size();++i)
+          hs->Add(histos[i]);
 
-    c->RedrawAxis();
-    return c;
+      l->SetFillColor(kWhite);
+      l->SetBorderSize(0);
+
+      TCanvas * c = new TCanvas(var,var,600,600);
+      c->cd();
+      hs->Draw("HIST,NOSTACK");
+      hs->GetXaxis()->SetTitle(labelX);
+      hs->GetYaxis()->SetLabelSize(0.04);
+      hs->GetXaxis()->SetLabelSize(0.04);
+      hs->GetXaxis()->SetTitleSize(0.04);
+      l->Draw();
+
+      c->RedrawAxis();
+      return c;
   }
 
 
@@ -661,119 +665,119 @@ public:
 
   TCanvas* makeStackedPlotAndSave(TString var,TString selection,TString lumi, int bins, float min, float max,TString labelX,TString units,double xLabel,double yLabel,double yLabel2,double xLegend,double yLegend,bool log,double ymin,double ymax,TString postFix = "" )
   {
-    THStack *hs = new THStack("hs","CMS Preliminary 2010");
-    TLegend *l = new TLegend(xLegend,yLegend,xLegend+0.3,yLegend+0.3);
-    TFile *f = new TFile("histograms.root","RECREATE");
-    f->cd();
+      THStack *hs = new THStack("hs","CMS Preliminary 2010");
+      TLegend *l = new TLegend(xLegend,yLegend,xLegend+0.3,yLegend+0.3);
+      TFile *f = new TFile("histograms.root","RECREATE");
+      f->cd();
 
-    float S=0.;
-    float B=0.;
-    float DATA=0.0;
+      float S=0.;
+      float B=0.;
+      float DATA=0.0;
 
-     std::vector<TH1F*> histos;
-    for(unsigned int i=0;i<trees_.size();++i)
+      std::vector<TH1F*> histos;
+      for(unsigned int i=0;i<trees_.size();++i)
       {
-	TH1F * hh = new TH1F("hh","",bins,min,max);
-	hh->Sumw2();
-	if(types_[i]<=0){//MC
-	  trees_[i]->Draw(var +">>hh",lumi+"*"+preselection_[i]+"*("+selection+")","goff");
-	  hh->SetLineWidth(2);
-	  hh->SetFillColor(colors_[i]);
-	  hh->SetFillStyle(styles_[i]);
+          TH1F * hh = new TH1F("hh","",bins,min,max);
+          hh->Sumw2();
+          if(types_[i]<=0){//MC
+              trees_[i]->Draw(var +">>hh",lumi+"*"+preselection_[i]+"*("+selection+")","goff");
+              hh->SetLineWidth(2);
+              hh->SetFillColor(colors_[i]);
+              hh->SetFillStyle(styles_[i]);
 
-	  hh->SetLineColor(kBlack);
-	  hh->SetName("hh"+var);
-	  if(types_[i]==-1) 
-	    S+=hh->Integral();
-	  else if(types_[i]==0)
-	    B+=hh->Integral();
-	  l->AddEntry(hh,labels_[i],"f");
-	  histos.push_back(hh);
+              hh->SetLineColor(kBlack);
+              hh->SetName("hh"+var);
+              if(types_[i]==-1) 
+                  S+=hh->Integral();
+              else if(types_[i]==0)
+                  B+=hh->Integral();
+              l->AddEntry(hh,labels_[i],"f");
+              histos.push_back(hh);
 
-	  hh->SetName(labels_[i]);
-	  hh->Write();
-	  
-	}
-	else {
-	  trees_[i]->Draw(var +">>hh",preselection_[i]+"*("+selection+")","goff");
-	  hh->SetMarkerStyle(20);
-	  l->AddEntry(hh,labels_[i],"p");
-	  histos.push_back(hh);
-	  DATA+=hh->Integral();
+              hh->SetName(labels_[i]);
+              hh->Write();
 
-	  hh->SetName(labels_[i]);
-	  hh->Write();
-	}
+          }
+          else {
+              trees_[i]->Draw(var +">>hh",preselection_[i]+"*("+selection+")","goff");
+              hh->SetMarkerStyle(20);
+              l->AddEntry(hh,labels_[i],"p");
+              histos.push_back(hh);
+              DATA+=hh->Integral();
+
+              hh->SetName(labels_[i]);
+              hh->Write();
+          }
       }
-    for(unsigned int i=0;i<histos.size();++i)
-      if(types_[i]<=0)
-	hs->Add(histos[i]);
-   
-    l->SetFillColor(kWhite);
-    l->SetBorderSize(0);
+      for(unsigned int i=0;i<histos.size();++i)
+          if(types_[i]<=0)
+              hs->Add(histos[i]);
 
-    
-    TCanvas *c;
-    if(log)
-      c = new TCanvas(var+"LOG"+postFix,var+"LOG"+postFix,600,600);
-    else
-      c = new TCanvas(var+postFix,var+postFix,600,600);
-
-    c->cd();
-    if(log)
-      c->SetLogy();
-
-    for(unsigned int i=0;i<trees_.size();++i)
-      if(types_[i]>0)//DATA
-      {
-	if(units=="")
-	  histos[i]->GetXaxis()->SetTitle(labelX);
-	else
-	  histos[i]->GetXaxis()->SetTitle(labelX+"["+units+"]");
-	  
-	TString s("Events / ");
-	char binW[200];
-	sprintf(binW,"%1.1f",histos[i]->GetBinWidth(1));
-	
-	s+=binW;
-	s+=" "+units;
-	histos[i]->GetYaxis()->SetTitle(s);
-	histos[i]->Draw("e1");
-	histos[i]->GetYaxis()->SetRangeUser(ymin,ymax);
-      }
-    
-
-    printf("C\n");  
-    hs->Draw("A,HIST,SAME");
-
-    TLatex *l1 = new TLatex(xLabel,yLabel,"CMS Preliminary 2010");
-    l1->SetTextSize(0.04);
-    l1->Draw();
-    TLatex *l2 = new TLatex(xLabel,yLabel2,"L_{int} = "+lumi+" pb^{-1}, #sqrt{s} = 7 TeV");
-    l2->SetTextSize(0.04);
-    l2->Draw();
+      l->SetFillColor(kWhite);
+      l->SetBorderSize(0);
 
 
-    for(unsigned int i=0;i<trees_.size();++i)
-      if(types_[i]>0)//DATA
-      {
-	histos[i]->Draw("e1,SAME");
-      }
-    l->Draw();
+      TCanvas *c;
+      if(log)
+          c = new TCanvas(var+"LOG"+postFix,var+"LOG"+postFix,600,600);
+      else
+          c = new TCanvas(var+postFix,var+postFix,600,600);
+
+      c->cd();
+      if(log)
+          c->SetLogy();
+
+      for(unsigned int i=0;i<trees_.size();++i)
+          if(types_[i]>0)//DATA
+          {
+              if(units=="")
+                  histos[i]->GetXaxis()->SetTitle(labelX);
+              else
+                  histos[i]->GetXaxis()->SetTitle(labelX+"["+units+"]");
+
+              TString s("Events / ");
+              char binW[200];
+              sprintf(binW,"%1.1f",histos[i]->GetBinWidth(1));
+
+              s+=binW;
+              s+=" "+units;
+              histos[i]->GetYaxis()->SetTitle(s);
+              histos[i]->Draw("e1");
+              histos[i]->GetYaxis()->SetRangeUser(ymin,ymax);
+          }
 
 
-    printf("S = %f \n",S);
-    printf("B = %f \n",B);
-    printf("MC = %f \n",S+B);
-    printf("DATA = %f \n",DATA);
-    printf("Purity = S/(S+B) = %f\n",S/(S+B));
-    printf("Significance1 = S/SQRT(S+B) = %f\n",S/sqrt(S+B));
-    printf("Significance2 = S/SQRT(B) = %f\n",S/sqrt(B));
-   
+      printf("C\n");  
+      hs->Draw("A,HIST,SAME");
 
-    f->Close();
+      TLatex *l1 = new TLatex(xLabel,yLabel,"CMS Preliminary 2010");
+      l1->SetTextSize(0.04);
+      l1->Draw();
+      TLatex *l2 = new TLatex(xLabel,yLabel2,"L_{int} = "+lumi+" pb^{-1}, #sqrt{s} = 7 TeV");
+      l2->SetTextSize(0.04);
+      l2->Draw();
 
-    return c;
+
+      for(unsigned int i=0;i<trees_.size();++i)
+          if(types_[i]>0)//DATA
+          {
+              histos[i]->Draw("e1,SAME");
+          }
+      l->Draw();
+
+
+      printf("S = %f \n",S);
+      printf("B = %f \n",B);
+      printf("MC = %f \n",S+B);
+      printf("DATA = %f \n",DATA);
+      printf("Purity = S/(S+B) = %f\n",S/(S+B));
+      printf("Significance1 = S/SQRT(S+B) = %f\n",S/sqrt(S+B));
+      printf("Significance2 = S/SQRT(B) = %f\n",S/sqrt(B));
+
+
+      f->Close();
+
+      return c;
   }
 
 
@@ -781,144 +785,144 @@ public:
 
   TCanvas* makeStackedPlotR(TString var,TString selection,TString lumi, int bins, float min, float max,TString labelX,TString units,double xLabel,double yLabel,double yLabel2,double xLegend,double yLegend,bool log,double ymin,double ymax,TString postFix = "" )
   {
-    THStack *hs = new THStack("hs","CMS Preliminary 2010");
-    TLegend *l = new TLegend(xLegend,yLegend,xLegend+0.3,yLegend+0.3);
-    
-    float S=0.;
-    float B=0.;
-    float DATA=0.0;
+      THStack *hs = new THStack("hs","CMS Preliminary 2010");
+      TLegend *l = new TLegend(xLegend,yLegend,xLegend+0.3,yLegend+0.3);
 
-     std::vector<TH1F*> histos;
-    for(unsigned int i=0;i<trees_.size();++i)
+      float S=0.;
+      float B=0.;
+      float DATA=0.0;
+
+      std::vector<TH1F*> histos;
+      for(unsigned int i=0;i<trees_.size();++i)
       {
-	TH1F * hh = new TH1F("hh","",bins,min,max);
-	hh->Sumw2();
-	if(types_[i]<=0){//MC
-	  trees_[i]->Draw(var +">>hh",lumi+"*"+preselection_[i]+"*("+selection+")","goff");
-	  hh->SetLineWidth(2);
-	  hh->SetFillColor(colors_[i]);
-	  hh->SetFillStyle(styles_[i]);
-	  hh->SetLineColor(kBlack);
-	  hh->SetName("hh"+var);
-	  if(types_[i]==-1)
-	    S+=hh->Integral();
-	  else if(types_[i]==0)
-	    B+=hh->Integral();
-	  l->AddEntry(hh,labels_[i],"f");
-	  histos.push_back(hh);
+          TH1F * hh = new TH1F("hh","",bins,min,max);
+          hh->Sumw2();
+          if(types_[i]<=0){//MC
+              trees_[i]->Draw(var +">>hh",lumi+"*"+preselection_[i]+"*("+selection+")","goff");
+              hh->SetLineWidth(2);
+              hh->SetFillColor(colors_[i]);
+              hh->SetFillStyle(styles_[i]);
+              hh->SetLineColor(kBlack);
+              hh->SetName("hh"+var);
+              if(types_[i]==-1)
+                  S+=hh->Integral();
+              else if(types_[i]==0)
+                  B+=hh->Integral();
+              l->AddEntry(hh,labels_[i],"f");
+              histos.push_back(hh);
 
-	}
-	else {
-	  trees_[i]->Draw(var +">>hh",preselection_[i]+"*("+selection+")","goff");
-	  hh->SetMarkerStyle(20);
-	  l->AddEntry(hh,labels_[i],"p");
-	  histos.push_back(hh);
-	  DATA+=hh->Integral();
-	}
+          }
+          else {
+              trees_[i]->Draw(var +">>hh",preselection_[i]+"*("+selection+")","goff");
+              hh->SetMarkerStyle(20);
+              l->AddEntry(hh,labels_[i],"p");
+              histos.push_back(hh);
+              DATA+=hh->Integral();
+          }
       }
-    for(unsigned int i=0;i<histos.size();++i)
-      if(types_[i]<=0)
-	hs->Add(histos[i]);
-   
-    l->SetFillColor(kWhite);
-    l->SetBorderSize(0);
+      for(unsigned int i=0;i<histos.size();++i)
+          if(types_[i]<=0)
+              hs->Add(histos[i]);
 
-    
-    TCanvas *c  = new TCanvas(var+"LOG"+postFix,var+"LOG"+postFix,600,900);
-;
- c->cd();
-    TPad * plotPad = new TPad("pad1","",0.0,0.3,1.0,1.0);
-    TPad * ratioPad = new TPad("pad2","",0.0,0.0,1.0,0.3);
-
-    plotPad->Draw();
-    ratioPad->Draw();
-    plotPad->cd();
-
-    if(log)
-      c->cd(1)->SetLogy();
-
-    for(unsigned int i=0;i<trees_.size();++i)
-      if(types_[i]>0)//DATA
-      {
-	if(units=="")
-	  histos[i]->GetXaxis()->SetTitle(labelX);
-	else
-	  histos[i]->GetXaxis()->SetTitle(labelX+"["+units+"]");
-	  
-	TString s("Events / ");
-	char binW[200];
-	sprintf(binW,"%1.1f",histos[i]->GetBinWidth(1));
-	
-	s+=binW;
-	s+=" "+units;
-	histos[i]->GetYaxis()->SetTitle(s);
-	histos[i]->Draw("e1");
-	histos[i]->GetYaxis()->SetRangeUser(ymin,ymax);
-      }
-    
-
-    printf("C\n");  
-    hs->Draw("A,HIST,SAME");
-
-    TLatex *l1 = new TLatex(xLabel,yLabel,"CMS Preliminary 2010");
-    l1->SetTextSize(0.04);
-    l1->Draw();
-    TLatex *l2 = new TLatex(xLabel,yLabel2,"L_{int} = "+lumi+" pb^{-1}, #sqrt{s} = 7 TeV");
-    l2->SetTextSize(0.04);
-    l2->Draw();
+      l->SetFillColor(kWhite);
+      l->SetBorderSize(0);
 
 
-    for(unsigned int i=0;i<trees_.size();++i)
-      if(types_[i]>0)//DATA
-      {
-	histos[i]->Draw("e1,SAME");
-      }
-    l->Draw();
+      TCanvas *c  = new TCanvas(var+"LOG"+postFix,var+"LOG"+postFix,600,900);
+      ;
+      c->cd();
+      TPad * plotPad = new TPad("pad1","",0.0,0.3,1.0,1.0);
+      TPad * ratioPad = new TPad("pad2","",0.0,0.0,1.0,0.3);
 
-    ratioPad->cd();
+      plotPad->Draw();
+      ratioPad->Draw();
+      plotPad->cd();
 
-    TH1F * mcAll = new TH1F("mcAll","",bins,min,max);
-    TH1F * dataAll = new TH1F("dataAll","",bins,min,max);
+      if(log)
+          c->cd(1)->SetLogy();
 
+      for(unsigned int i=0;i<trees_.size();++i)
+          if(types_[i]>0)//DATA
+          {
+              if(units=="")
+                  histos[i]->GetXaxis()->SetTitle(labelX);
+              else
+                  histos[i]->GetXaxis()->SetTitle(labelX+"["+units+"]");
 
-    for(unsigned int i=0;i<histos.size();++i)
-      if(types_[i]<=0)//MC
-	mcAll->Add(histos[i]);
-      else
-	dataAll->Add(histos[i]);
+              TString s("Events / ");
+              char binW[200];
+              sprintf(binW,"%1.1f",histos[i]->GetBinWidth(1));
 
-    
-    
-    dataAll->Divide(dataAll,mcAll);
-    dataAll->SetMarkerStyle(20);
-    dataAll->Fit("pol0","SAME");
-    
-
-//     if(units=="")
-//       mcAll->GetXaxis()->SetTitle(labelX);
-//     else
-//       mcAll->GetXaxis()->SetTitle(labelX+"["+units+"]");
-
-
-    dataAll->GetYaxis()->SetTitle("DATA/MC");
-    dataAll->GetYaxis()->SetTitleOffset(0.55);
-    dataAll->GetYaxis()->SetTitleSize(0.12);
-    dataAll->GetYaxis()->SetLabelSize(0.12);
-    dataAll->GetXaxis()->SetLabelSize(0.12);
-    dataAll->GetYaxis()->SetNdivisions(5);
-    dataAll->GetYaxis()->SetRangeUser(0.,2.);
-
-    dataAll->Draw("P");
+              s+=binW;
+              s+=" "+units;
+              histos[i]->GetYaxis()->SetTitle(s);
+              histos[i]->Draw("e1");
+              histos[i]->GetYaxis()->SetRangeUser(ymin,ymax);
+          }
 
 
-    printf("S = %f \n",S);
-    printf("B = %f \n",B);
-    printf("Purity = S/(S+B) = %f\n",S/(S+B));
-    printf("Significance1 = S/SQRT(S+B) = %f\n",S/sqrt(S+B));
-    printf("Significance2 = S/SQRT(B) = %f\n",S/sqrt(B));
-    c->RedrawAxis();
+      printf("C\n");  
+      hs->Draw("A,HIST,SAME");
 
-    return c;
+      TLatex *l1 = new TLatex(xLabel,yLabel,"CMS Preliminary 2010");
+      l1->SetTextSize(0.04);
+      l1->Draw();
+      TLatex *l2 = new TLatex(xLabel,yLabel2,"L_{int} = "+lumi+" pb^{-1}, #sqrt{s} = 7 TeV");
+      l2->SetTextSize(0.04);
+      l2->Draw();
+
+
+      for(unsigned int i=0;i<trees_.size();++i)
+          if(types_[i]>0)//DATA
+          {
+              histos[i]->Draw("e1,SAME");
+          }
+      l->Draw();
+
+      ratioPad->cd();
+
+      TH1F * mcAll = new TH1F("mcAll","",bins,min,max);
+      TH1F * dataAll = new TH1F("dataAll","",bins,min,max);
+
+
+      for(unsigned int i=0;i<histos.size();++i)
+          if(types_[i]<=0)//MC
+              mcAll->Add(histos[i]);
+          else
+              dataAll->Add(histos[i]);
+
+
+
+      dataAll->Divide(dataAll,mcAll);
+      dataAll->SetMarkerStyle(20);
+      dataAll->Fit("pol0","SAME");
+
+
+      //     if(units=="")
+      //       mcAll->GetXaxis()->SetTitle(labelX);
+      //     else
+      //       mcAll->GetXaxis()->SetTitle(labelX+"["+units+"]");
+
+
+      dataAll->GetYaxis()->SetTitle("DATA/MC");
+      dataAll->GetYaxis()->SetTitleOffset(0.55);
+      dataAll->GetYaxis()->SetTitleSize(0.12);
+      dataAll->GetYaxis()->SetLabelSize(0.12);
+      dataAll->GetXaxis()->SetLabelSize(0.12);
+      dataAll->GetYaxis()->SetNdivisions(5);
+      dataAll->GetYaxis()->SetRangeUser(0.,2.);
+
+      dataAll->Draw("P");
+
+
+      printf("S = %f \n",S);
+      printf("B = %f \n",B);
+      printf("Purity = S/(S+B) = %f\n",S/(S+B));
+      printf("Significance1 = S/SQRT(S+B) = %f\n",S/sqrt(S+B));
+      printf("Significance2 = S/SQRT(B) = %f\n",S/sqrt(B));
+      c->RedrawAxis();
+
+      return c;
   }
 
 
