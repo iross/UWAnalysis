e294bf376eb62380e8f4e40ebceafa22fa09a012
Austin Belknap <dabelknap@wisc.edu>
2012-07-18 09:22:02 -0500

    Implemented Cuts for ESEE

--- ROOT/macros/plotScripts/makeAllZZESEEPlots.C
+++ ROOT/macros/plotScripts/makeAllZZESEEPlots.C
@@ -7,6 +7,7 @@
 
     double ymax = 3000;
 
+	//TCanvas * esee = ESEEplotter->makeStackedPlotMC("mass",selection,lumi,20,100,160,"M_{4l}","GeV/c^{2}",61,ymax*0.9,ymax*0.85,0.6,0.6,false,0.0001,ymax);
 	//TCanvas * esee = ESEEplotter->makeStackedPlotMC("z1Mass",selection,lumi,20,50,200,"M_{4l}","GeV/c^{2}",61,ymax*0.9,ymax*0.85,0.6,0.6,false,0.0001,ymax);
 
     //double ymax = 10000;
@@ -14,18 +15,18 @@
 
     //std::string selection = "z1Charge == 0 && z2l2isEE && z1l1Pt > 10 && z1l2Pt > 10 && z2l1Pt > 12 && z2l2Pt > 5 && z2Mass > 10";
     selection += "&& z1l1MissHits < 1 && z2l2MissHits < 1 && z2l1MissHits < 1";
-    selection += "&& z1l1CiCLoose&9 == 9 && z2l2CiCTight&9 == 9 && z2l1CiCTight&9 == 9";
+    selection += "&& z1l1CiCLoose&9 == 9 && z2l2CiCTight&9 == 9 && z2l1CiCTight&9 == 9 && z1l1Pt > 10 && z1l2Pt > 10";
     //selection += "&& 70 < z1Mass && z1Mass < 120";
 
 	//gROOT->ProcessLine(".!mkdir EEEMPlots");
 
-    TCanvas* test = ESEEplotter->makeComparison("z1l1Pt",selection,"1",25,0,150,"p_{T} [GeV/c]");
-    TCanvas* test = ESEEplotter->makeComparison("z1l2Pt",selection,"1",25,0,150,"p_{T} [GeV/c]");
-    TCanvas* test = ESEEplotter->makeComparison("z2l1Pt",selection,"1",25,0,150,"p_{T} [GeV/c]");
-    TCanvas* test = ESEEplotter->makeComparison("z2l2Pt",selection,"1",25,0,150,"p_{T} [GeV/c]");
-    TCanvas* test = ESEEplotter->makeComparison("z1Mass",selection,"1",20,40,120,"M_{ee}");
-    TCanvas* test = ESEEplotter->makeComparison("z2Mass",selection,"1",20,40,120,"M_{ee}");
-    TCanvas* test = ESEEplotter->makeComparison("mass",selection,"1",20,100,200,"M_{ee}");
+    //TCanvas* test = ESEEplotter->makeComparison("z1l1Pt",selection,"1",25,0,150,"p_{T} [GeV/c]");
+    //TCanvas* test = ESEEplotter->makeComparison("z1l2Pt",selection,"1",25,0,150,"p_{T} [GeV/c]");
+    //TCanvas* test = ESEEplotter->makeComparison("z2l1Pt",selection,"1",25,0,150,"p_{T} [GeV/c]");
+    //TCanvas* test = ESEEplotter->makeComparison("z2l2Pt",selection,"1",25,0,150,"p_{T} [GeV/c]");
+    //TCanvas* test = ESEEplotter->makeComparison("z1Mass",selection,"1",20,40,120,"M_{ee}");
+    //TCanvas* test = ESEEplotter->makeComparison("z2Mass",selection,"1",20,40,120,"M_{ee}");
+    //TCanvas* test = ESEEplotter->makeComparison("mass",selection,"1",20,100,200,"M_{ee}");
 
     //TCanvas* test = ESEEplotter->makeComparison("z1l1Eta",selection,"1",40,-4,4,"#eta");
     //TCanvas* test = ESEEplotter->makeComparison("z1l2Eta",selection,"1",40,-4,4,"#eta");
@@ -35,62 +36,46 @@
     //TCanvas* test = ESEEplotter->makeComparison("z1l1RelPfIsoRho",selection,"1",10,0,0.5,"Rel Pf Iso");
     //TCanvas* test = ESEEplotter->makeComparison("z2l2RelPfIsoRho",selection,"1",10,0,0.5,"Rel Pf Iso");
     //TCanvas* test = ESEEplotter->makeComparison("z2l1RelPfIsoRho",selection,"1",10,0,0.5,"Rel Pf Iso");
-    //selection += "&& z1l1RelPfIsoRho < 0.2 && z2l2RelPfIsoRho < 0.2 && z2l1RelPfIsoRho < 0.2";
+    selection += "&& z1l1RelPfIsoRho < 0.4 && z2l2RelPfIsoRho < 0.4 && z2l1RelPfIsoRho < 0.4";
     
-    //TCanvas* test = ESEEplotter->makeComparison("z2l2sigmaIetaIeta",selection,"1",20,0,0.08,"sigma iEta iEta");
-    //TCanvas* test = ESEEplotter->makeComparison("z2l2e1x5/z2l2e5x5",selection,"1",25,0,1,"e1x5/e5x5");
-    //TCanvas* test = ESEEplotter->makeComparison("z2l2e2x5/z2l2e5x5",selection,"1",25,0,1,"e2x5/e5x5");
+    TCanvas* test = ESEEplotter->makeComparison("z1l2HE",selection,"1",20,0,1,"H/E");
+    selection += "&& z1l2HE < 0.1";
+
+    TCanvas* test = ESEEplotter->makeComparison("z1l2sigmaIetaIeta",selection,"1",20,0,0.08,"sigma iEta iEta");
+    //TCanvas* test = ESEEplotter->makeComparison("z1l2e1x5/z1l2e5x5",selection,"1",25,0,1,"e1x5/e5x5");
+    //TCanvas* test = ESEEplotter->makeComparison("z1l2e2x5/z1l2e5x5",selection,"1",25,0,1,"e2x5/e5x5");
+    selection += "&& z1l2sigmaIetaIeta < 0.04";
 
     //TCanvas* test = ESEEplotter->makeComparison("mass",selection,"1",25,100,300,"M_{4l} [GeV/c^{2}]");
 
-    //selection += "&& z2l2hcalTowerSumEtConeDR03 < 10";
-    //TCanvas* test = ESEEplotter->makeComparison("z1l2ecalRecHitSumEtDR03",selection,"1",20,0,50,"ecalRecHitEtDR03 [GeV]");
-    //TCanvas* test = ESEEplotter->makeComparison("z1l2hcalTowerSumEtConeDR03",selection,"1",20,0,40,"hcalTowerSumEtConeDR03 [GeV]");
-    //TCanvas* test = ESEEplotter->makeComparison("z1l2hcalDepth1TowerSumEtConeDR03",selection,"1",20,0,40,"hcalDepth1TowerSumEtConeDR03 [GeV]");
+    //TCanvas* test = ESEEplotter->makeComparison("z1Mass",selection,"1",20,40,120,"M_{eSC}");
+    //TCanvas* test = ESEEplotter->makeComparison("z2Mass",selection,"1",20,40,120,"M_{ee}");
+
+    //selection += "&& z1l2hcalDepth2TowerSumEtConeDR03 < 0.5";
+    //selection += "&& z1l2hcalDepth1TowerSumEtConeDR03 < 2";
+    //selection += "&& z1l2hcalTowerSumEtConeDR03 < 2";
+    //selection += "&& z1l2ecalRecHitSumEtDR03 < 2";
+    //TCanvas* test = ESEEplotter->makeComparison("z1l2ecalRecHitSumEtDR03",selection,"1",20,0,20,"ecalRecHitEtDR03 [GeV]");
+    //TCanvas* test = ESEEplotter->makeComparison("z1l2hcalTowerSumEtConeDR03",selection,"1",20,0,20,"hcalTowerSumEtConeDR03 [GeV]");
+    //TCanvas* test = ESEEplotter->makeComparison("z1l2hcalDepth1TowerSumEtConeDR03",selection,"1",20,0,20,"hcalDepth1TowerSumEtConeDR03 [GeV]");
     //TCanvas* test = ESEEplotter->makeComparison("z1l2hcalDepth2TowerSumEtConeDR03",selection,"1",20,0,5,"hcalDepth2TowerSumEtConeDR03 [GeV]");
 
-    //TCanvas* test = ESEEplotter->makeComparison("z2l2ecalRecHitSumEtDR03/z2l2Pt",selection,"1",20,0,1,"");
-    //TCanvas* test = ESEEplotter->makeComparison("z2l2hcalTowerSumEtConeDR03/z2l2Pt",selection,"1",20,0,1,"");
-    //TCanvas* test = ESEEplotter->makeComparison("z2l2hcalDepth1TowerSumEtConeDR03/z2l2Pt",selection,"1",20,0,1,"");
-    //TCanvas* test = ESEEplotter->makeComparison("z2l2hcalDepth2TowerSumEtConeDR03/z2l2Pt",selection,"1",20,0,1,"");
+    TCanvas* test = ESEEplotter->makeComparison("z1l2ecalRecHitSumEtDR03/z1l2Pt",selection,"1",25,0,1,"");
+    selection += "&& z1l2ecalRecHitSumEtDR03/z1l2Pt < 0.05";
+    //selection += "&& z1l2hcalTowerSumEtConeDR03/z1l2Pt < 0.07";
+    //TCanvas* test = ESEEplotter->makeComparison("z1l2ecalRecHitSumEtDR03/z1l2Pt",selection,"1",25,0,1,"");
+    //TCanvas* test = ESEEplotter->makeComparison("z1l2hcalTowerSumEtConeDR03/z1l2Pt",selection,"1",25,0,1,"");
+    //TCanvas* test = ESEEplotter->makeComparison("z1l2hcalDepth1TowerSumEtConeDR03/z1l2Pt",selection,"1",25,0,1,"");
+    //TCanvas* test = ESEEplotter->makeComparison("z1l2hcalDepth2TowerSumEtConeDR03/z1l2Pt",selection,"1",25,0,1,"");
 
     //selection += "&& z2l2ecalRecHitSumEtDR03";
+
+    TCanvas* test = ESEEplotter->makeComparison("z2Mass",selection,"1",20,40,120,"M_{ee}");
+    selection += "&& z2Mass < 70";
     
-    //double ymax = 10000;
-	//TCanvas * esee = ESEEplotter->makeStackedPlotMC("mass",selection,lumi,20,50,200,"M_{4l}","GeV/c^{2}",61,ymax*0.9,ymax*0.85,0.6,0.6,false,0.0001,ymax);
+    TCanvas* test = ESEEplotter->makeComparison("z1Mass",selection,"1",20,40,120,"M_{eSC}");
+    double ymax = 5;
+	TCanvas * esee = ESEEplotter->makeStackedPlotMC("mass",selection,lumi,6,100,160,"M_{4l}","GeV/c^{2}",61,ymax*0.9,ymax*0.85,0.6,0.6,false,0.0001,ymax);
 
-    //TCanvas* test = ESEEplotter->makeComparison("z1Mass",selection,"1",40,0,120,"M_{ee}");
-    //TCanvas* test = ESEEplotter->makeComparison("z2Mass",selection,"1",40,0,120,"M_{ee}");
-
-	//double ymax=4000.0;
-	//TCanvas * esee = ESEEplotter->makeStackedPlotMC("z2l2Pt",selection,lumi,20,0,100,"M_{ee}","GeV/c^{2}",61,ymax*0.9,ymax*0.85,0.6,0.6,false,0.0001,ymax);
-	//TCanvas * esee = ESEEplotter->makeStackedPlotMC("z1l2Eta",selection,lumi,20,-4.0,4.0,"M_{ee}","GeV/c^{2}",61,ymax*0.9,ymax*0.85,0.6,0.6,false,0.0001,ymax);
-	//TCanvas * esee = ESEEplotter->makeStackedPlotMC("z1Mass",selection,lumi,10,40,120,"M_{ee}","GeV/c^{2}",61,ymax*0.9,ymax*0.85,0.6,0.6,false,0.0001,ymax);
-
-	//TCanvas * esee = ESEEplotter->makeStackedPlotMC("z1l1RelPfIsoRho",selection,lumi,10,0,1,"M_{ee}","GeV/c^{2}",61,ymax*0.9,ymax*0.85,0.6,0.6,false,0.0001,ymax);
-    //selection += "&& z1l1RelPfIsoRho < 0.2";
-	//TCanvas * esee = ESEEplotter->makeStackedPlotMC("z1l2RelPfIsoRho",selection,lumi,10,0,1,"M_{ee}","GeV/c^{2}",61,ymax*0.9,ymax*0.85,0.6,0.6,false,0.0001,ymax);
-    //selection += "&& z1l2RelPfIsoRho < 0.2";
-	//TCanvas * esee = ESEEplotter->makeStackedPlotMC("z2l1RelPfIsoRho",selection,lumi,20,0,2,"M_{ee}","GeV/c^{2}",61,ymax*0.9,ymax*0.85,0.6,0.6,false,0.0001,ymax);
-    //selection += "&& z2l1RelPfIsoRho < 0.2";
-
-    //ymax = 40;
-	//TCanvas * esee = ESEEplotter->makeStackedPlotMC("z2l2sigmaIetaIeta",selection,lumi,25,0,0.08,"M_{ee}","GeV/c^{2}",61,ymax*0.9,ymax*0.85,0.6,0.6,false,0.0001,ymax);
-	//TCanvas * esee = ESEEplotter->makeStackedPlotMC("z2l2e1x5/z2l2e5x5",selection,lumi,25,0,1.0,"M_{ee}","GeV/c^{2}",61,ymax*0.9,ymax*0.85,0.6,0.6,false,0.0001,ymax);
-	//TCanvas * esee = ESEEplotter->makeStackedPlotMC("z2l2e2x5/z2l2e5x5",selection,lumi,25,0,1.0,"M_{ee}","GeV/c^{2}",61,ymax*0.9,ymax*0.85,0.6,0.6,false,0.0001,ymax);
-	
-
-    //selection += "&& ( z2l2e1x5/z2l2e5x5 > 0.3 || z2l2e2x5/z2l2e5x5 > 0.6 ) && z2l2hcalDepth2TowerSumEtConeDR03 < 1 && z2l2ecalRecHitSumEtDR03 < 3.5";
-
-    //ymax = 200;
-    //TCanvas * esee = ESEEplotter->makeStackedPlotMC("z2l2hcalDepth1TowerSumEtConeDR03",selection,lumi,20,0,40,"M_{ee}","GeV/c^{2}",61,ymax*0.9,ymax*0.85,0.6,0.6,false,0.0001,ymax);
-    //TCanvas * esee = ESEEplotter->makeStackedPlotMC("z2l2hcalDepth2TowerSumEtConeDR03",selection,lumi,20,0,5,"M_{ee}","GeV/c^{2}",61,ymax*0.9,ymax*0.85,0.6,0.6,false,0.0001,ymax);
-    //TCanvas * esee = ESEEplotter->makeStackedPlotMC("z2l2ecalRecHitSumEtDR03",selection,lumi,20,0,50,"M_{ee}","GeV/c^{2}",61,ymax*0.9,ymax*0.85,0.6,0.6,false,0.0001,ymax);
-    //TCanvas * esee = ESEEplotter->makeStackedPlotMC("z2l2hcalTowerSumEtConeDR03",selection,lumi,20,0,40,"M_{ee}","GeV/c^{2}",61,ymax*0.9,ymax*0.85,0.6,0.6,false,0.0001,ymax);
-
-	//TCanvas * esee = ESEEplotter->makeStackedPlotMC("z1Mass",selection,lumi,20,40,120,"M_{ee}","GeV/c^{2}",61,ymax*0.9,ymax*0.85,0.6,0.6,false,0.0001,ymax);
-  
-
-	//TCanvas * esee = ESEEplotter->makeStackedPlotMC("z2l2e1x5/z2l2e5x5",selection,lumi,25,0.0,1.0,"Shower Shape","e1x5/e5x5",61,ymax*0.9,ymax*0.85,0.6,0.6,false,0.0001,ymax);
-	//TCanvas * esee = ESEEplotter->makeStackedPlotMC("z2l2sigmaIetaIeta",selection,lumi,50,0.0,0.08,"Shower Shape","sigmaIetaIeta",61,ymax*0.9,ymax*0.85,0.6,0.6,false,0.0001,ymax);
+    //std::cout << selection << std::endl;
 }
--- ROOT/macros/plotters/loadEleSCEleElePlotter.C
+++ ROOT/macros/plotters/loadEleSCEleElePlotter.C
@@ -8,11 +8,11 @@
     //ESEEplotter->addFile("eleSCEleEleEventTree/eventTree","/afs/hep.wisc.edu/cms/belknap/UWTest/src/UWAnalysis/dataSamples/ZJets.root","Z+Jets","__WEIGHT__",0,kViolet-5,kViolet+3);
     //ESEEplotter->addFile("eleSCEleEleEventTree/eventTree","/afs/hep.wisc.edu/cms/belknap/UWTest/src/UWAnalysis/dataSamples/ggHZZ4l.root","H(125) #rightarrow ZZ","__WEIGHT__",-1,kOrange-2,kBlack);
 
-    ESEEplotter->addFile("eleSCEleEleEventTree/eventTree","/Users/austin/Desktop/HiggsZZ/dataSamples/ZJets.root","Z+Jets","__WEIGHT__",0,kViolet-5,kViolet+3);
+    ESEEplotter->addFile("eleSCEleEleEventTree/eventTree","/Users/austin/Desktop/HiggsZZ/dataSamples/ZJets.root","Z+Jets","__WEIGHT__noPU",0,kViolet-5,kViolet+3);
     //ESEEplotter->addFile("eleSCEleEleEventTree/eventTree","/Users/austin/Desktop/HiggsZZ/dataSamples/TTJets.root","t#bar{t}","__WEIGHT__",0,kGreen+2,kGreen+2);
     ESEEplotter->addFile("eleSCEleEleEventTree/eventTree","/Users/austin/Desktop/HiggsZZ/dataSamples/WZTo3LNu.root","WZ","__WEIGHT__",0,kRed+2,kRed+4);
     ESEEplotter->addFile("eleSCEleEleEventTree/eventTree","/Users/austin/Desktop/HiggsZZ/dataSamples/ZZ4l.root","EWK ZZ","__WEIGHT__",0,kOrange+7,kOrange+3);
-    ESEEplotter->addFile("eleSCEleEleEventTree/eventTree","/Users/austin/Desktop/HiggsZZ/dataSamples/ggHZZ4l.root","H(125) #rightarrow ZZ","__WEIGHT__",-1,kBlue-4,kBlack);
+    ESEEplotter->addFile("eleSCEleEleEventTree/eventTree","/Users/austin/Desktop/HiggsZZ/dataSamples/ggHZZ4l.root","H(125) #rightarrow ZZ","__WEIGHT__ * ( abs(gz1l1PdgId) == 11 && abs(gz1l2PdgId) == 11 && abs(gz2l1PdgId) == 11 && abs(gz2l2PdgId) == 11)",-1,kBlue-4,kBlack);
 
     //ESEEplotter->addFile("eleSCEleEleEventTree/eventTree","/Users/austin/Desktop/HiggsZZ/dataSamples/ZJets.root","Z+Jets","1",0,kViolet-5,kViolet+3);
     //ESEEplotter->addFile("eleSCEleEleEventTree/eventTree","/Users/austin/Desktop/HiggsZZ/dataSamples/TTJets.root","t#bar{t}","1",0,kGreen+2,kGreen+2);
