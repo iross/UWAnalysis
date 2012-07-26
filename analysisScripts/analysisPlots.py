f5e49ef97036108af682129793db1d3930960bc7
Ian Ross <iross@login06.hep.wisc.edu>
2012-07-26 09:21:51 -0500

    WIP: plotting overhaul

--- analysisScripts/analysisPlots.py
+++ analysisScripts/analysisPlots.py
@@ -1,4 +1,4 @@
-#!/Library/Frameworks/EPD64.framework/Versions/Current/bin//python
+
 from ROOT import *
 from simplePlots import *
 import math
@@ -6,6 +6,7 @@ import os
 from optparse import OptionParser
 
 ROOT.gROOT.ProcessLine(".X CMSStyle.C")
+ROOT.gROOT.SetBatch(True)
 
 def getTrees(file,postfix):
 	arr={}
@@ -86,156 +87,153 @@ def makeScatter(fd,year,lumi):
 		can.SaveAs(year+"/"+state+"/"+state+"_z1Mass_z2Mass.root")
 		can.SaveAs(year+"/"+state+"/"+state+"_z1Mass_z2Mass.C")
 
-def makePlots(dir="2012",postfix="8TeV",lumi="2.95",extra="1",var="z1Mass",varNice="Z_{M}^{1}",bins=range(60,120,5),texx=63,texyf=0.3,legx=0.2):
-	fd=TFile("DATA_12Jun_wMuEG_combed_plusFSR.root")
-	fd=TFile("DATAfinal.root")
-	d=getTrees(fd,"Cleaned")
+def makePlots(datasets,trees,dir="2012",postfix="8TeV",lumi="2.95",extra="1",var="z1Mass",varNice="Z_{M}^{1}",bins=range(60,120,5),texx=63,texyf=0.3,legx=0.2):
+    #	eventDump(d["eeee"],extra)
+#	eventDump(d["eemm"],extra)
+#	eventDump(d["mmmm"],extra)
 
-	eventDump(d["eeee"],extra)
-	eventDump(d["eemm"],extra)
-	eventDump(d["mmmm"],extra)
 
-	year=dir
-	if year=="2012":
-		l="8"
-	else:
-		l="7"
+    year=dir
+    if year=="2012":
+        l="8"
+    else:
+        l="7"
 
-#	fzz=TFile("ZZ_8TeV.root")
-#	fzz=TFile("ZZ_8TeV_wNoPU.root")
-#	fzz=TFile("ggZZ_combed.root")
-#	fzz=TFile("ZZ_combed_test.root")
-	fzz=TFile("qqZZ_combed.root")
-#	fzz=TFile("qqZZ_7TeV_combed.root")
-	zz=getTrees(fzz,"Cleaned")
+    data={}
+    hists={}
+    files={}
+    for dataset in datasets:
+        data[dataset]={}
+        hists[dataset]={}
+        files[dataset]=TFile(datasets[dataset]['file'])        
+        for tree in trees:
+            data[dataset][tree]=files[dataset].Get(trees[tree])
+            if datasets[dataset]['isMC']:
+                hists[dataset][tree]=makeHist(files[dataset].Get(trees[tree]),var,"("+extra+")*(__WEIGHT__noPU*"+lumi+"*1000)",50,100,600,False,True,bins)
+            else:
+                hists[dataset][tree]=makeHist(files[dataset].Get(trees[tree]),var,extra,50,100,600,False,True,bins)
+                hists[dataset][tree].GetXaxis().SetTitle(varNice)
+                if bins[0]-bins[1] == bins[len(bins)-2]-bins[len(bins)-1]: # if spaced evenly
+                    div=(float(bins[len(bins)-1])-float(bins[0]))/(len(bins)-1)
+                    hists[dataset][tree].GetYaxis().SetTitle("Events / %.0f GeV" %div)
+                else:
+                    hists[dataset][tree].GetYaxis().SetTitle("Events")
+            print dataset,'---',hists[dataset][tree].Integral()
+            hists[dataset][tree].SetFillColor(datasets[dataset]['color'])
 
-	fzj=TFile("BGStdIso_17May.root")
-	zj=getTrees(fzj,"_noIsoSS/eventTree")
-	
-	fh=TFile("ggH125_combed.root")
-	ht=getTrees(fh,"Cleaned")
+    #stack/style hists
 
-	can = TCanvas("can","can",600,600)
+#    plot(hists) #if data, do on top. Stack others. etc.
+    #legend
 
-	leg=TLegend(legx,0.6,legx+0.2,0.9)
+    can = TCanvas("can","can",600,600)
+    for tree in trees:
+        leg=TLegend(legx,0.6,legx+0.2,0.9)
+        leg.SetFillColor(kWhite)
+        if not os.path.exists(dir+"/"+tree):
+            os.makedirs(dir+"/"+tree)
+        hs=THStack("hs","stack")
+        for dataset in datasets:
+            if not datasets[dataset]['isMC']:
+                leg.AddEntry(hists[dataset][tree],"Data","p")
+            else:
+                hs.Add(hists[dataset][tree])
+                leg.AddEntry(hists[dataset][tree],dataset,"f")
+        print datasets
+        ymax=max(hists['DATA'][tree].GetMaximum(),hs.GetMaximum())
+        ymax=ceil(ymax+sqrt(ymax))
+        ymax=int(ymax)
+        l1 = TLatex(bins[0]+2,ymax*1.01,"CMS Preliminary "+year);
+        l1.SetTextSize(0.04);
+        l2 = TLatex(texx,texyf*ymax,"#splitline{L_{int} ="+lumi+" fb^{-1}}{#sqrt{s} = "+l+" TeV}");
+        l2.SetTextSize(0.04);
+        hists['DATA'][tree].GetYaxis().SetRangeUser(0,ymax)
+        hists['DATA'][tree].Draw('e1')
+        hs.Draw('hsame')
+        hists['DATA'][tree].Draw('e1,same')
+        leg.Draw()
+        l1.Draw()
+        l2.Draw()
+#        can.SetLogy(1)
+        if bins[0]-bins[1] == bins[len(bins)-2]-bins[len(bins)-1]: # if spaced evenly
+            can.SaveAs(dir+"/"+tree+"/"+tree+"_"+var+""+postfix+".png")
+            can.SaveAs(dir+"/"+tree+"/"+tree+"_"+var+""+postfix+".root")
+            can.SaveAs(dir+"/"+tree+"/"+tree+"_"+var+""+postfix+".C")
+        else:
+            can.SaveAs(dir+"/"+tree+"/"+tree+"_"+var+"_limitbinning"+postfix+".png")
+            can.SaveAs(dir+"/"+tree+"/"+tree+"_"+var+"_limitbinning"+postfix+".root")
+            can.SaveAs(dir+"/"+tree+"/"+tree+"_"+var+"_limitbinning"+postfix+".C")
 
-	dht=TH1F("dh","dh",len(bins)-1,array('d',bins))
-	dh={}
-	for tree in d:
-		t=d[tree]
-		dh[tree]=makeHist(t,var,extra,50,100,600,False,True,bins)
-		dht.Add(dh[tree])
-	dh["4l"]=dht
+#	#HACK! efficiencies taken as average of Matt's TP corrected yields/uncorrected
+#	eff={}
+#	eff["eeee"]=0.979
+#	eff["eemm"]=0.9885
+#	eff["mmmm"]=0.9876
+#	eff["4l"]=(eff["eeee"]+eff["mmmm"]+2.0*eff["eemm"])/4.0
+#	f=open(dir+"/yields.txt","w")
+#	for state in dh:
+#		f.write("---"+state+"---\n")
+#		f.write("Data: "+str(dh[state].Integral())+"\n")
+#		f.write("ZZ: "+str(zzh[state].Integral()*eff[state])+"\n")
+#		f.write("ZJets:"+str(zjh[state].Integral())+"\n")
+#		if not os.path.exists(dir+"/"+state):
+#			os.makedirs(dir+"/"+state)
+#		hs=THStack("hs","stack bg")
+#		hs.Add(zjh[state])
+#		hs.Add(zzh[state])
+#		hs.Add(hh[state])	
+#		print state,zzh[state].Integral()
+#		ymax=max(dh[state].GetMaximum(),hs.GetMaximum())
+#		ymax=ceil(ymax+sqrt(ymax))
+#		ymax=int(ymax)
+#		l1 = TLatex(bins[0]+2,ymax*1.01,"CMS Preliminary "+year);
+#		l1.SetTextSize(0.04);
+#		l2 = TLatex(texx,texyf*ymax,"#splitline{L_{int} ="+lumi+" fb^{-1}}{#sqrt{s} = "+l+" TeV}");
+#		l2.SetTextSize(0.04);
+#		dh[state].GetYaxis().SetRangeUser(0,ymax)
+#		dh[state].Draw()
+#		hs.Draw("hsame")
+#		dh[state].Draw("esame")
+#		leg.Draw()
+#		l1.Draw();
+#		l2.Draw();
+#		if bins[0]-bins[1] == bins[len(bins)-2]-bins[len(bins)-1]: # if spaced evenly
+#			can.SaveAs(dir+"/"+state+"/"+state+"_"+var+""+postfix+".png")
+#			can.SaveAs(dir+"/"+state+"/"+state+"_"+var+""+postfix+".root")
+#			can.SaveAs(dir+"/"+state+"/"+state+"_"+var+""+postfix+".C")
+#		else:
+#			can.SaveAs(dir+"/"+state+"/"+state+"_"+var+"_limitbinning"+postfix+".png")
+#			can.SaveAs(dir+"/"+state+"/"+state+"_"+var+"_limitbinning"+postfix+".root")
+#			can.SaveAs(dir+"/"+state+"/"+state+"_"+var+"_limitbinning"+postfix+".C")
+#	f.close()
 
-	zzht=TH1F("zzh","zzh",len(bins)-1,array('d',bins))
-	zzh={}
-	for tree in zz:
-		t=zz[tree]
-		zzh[tree]=makeHist(t,var,"("+extra+")*(weight*"+lumi+"*1000)",50,100,600,False,True,bins)
-	#	zzh[tree]=makeHist(t,var,"1*"+lumi+"*1000",50,100,600,False,True,bins)
-		zzht.Add(zzh[tree])
-		zzh[tree].SetFillColor(kAzure-9)
-		zzh[tree].SetMarkerSize(0.001)
-	zzh["4l"]=zzht
-	hht=TH1F("zzh","zzh",len(bins)-1,array('d',bins))
-	hh={}
-	for tree in ht:
-		t=ht[tree]
-		hh[tree]=makeHist(t,var,"("+extra+")*(weight*"+lumi+"*1000)",50,100,600,False,True,bins)
-	#	zzh[tree]=makeHist(t,var,"1*"+lumi+"*1000",50,100,600,False,True,bins)
-		hht.Add(hh[tree])
-		hh[tree].SetFillColor(kAzure-9)
-		hh[tree].SetMarkerSize(0.001)
-	hh["4l"]=hht
-	zjht=TH1F("zjh","zjh",len(bins)-1,array('d',bins))
-	zjh={}
-	for tree in zj:
-		t=zj[tree]
-		zjh[tree]=makeHist(t,var,extra+"&&z1l1Pt>5&&z1l2Pt>5&&z2l1Pt>5&&z2l2Pt>5",50,100,600,False,True,bins)
-		zjh[tree].SetFillColor(kGreen-5)
-		zjh[tree].SetMarkerSize(0.001)
-	zjh['mmmm'].Scale(0.52/zjh['mmmm'].Integral())
-	zjh['eemm'].Scale(0.44/zjh['eemm'].Integral())
-	zjh['eeee'].Scale(0.31/zjh['eeee'].Integral())
-	for hist in zjh:
-		zjht.Add(zjh[hist])
-	zjh['4l']=zjht
-
-	#colors, etc.
-	zzht.SetMarkerSize(0.001)
-	zzht.SetFillColor(kAzure-9)
-	hht.SetMarkerSize(0.001)
-	hht.SetFillColor(kWhite)
-	zjht.SetMarkerSize(0.001)
-	zjht.SetFillColor(kGreen-5)
-
-	#legend
-	leg.SetFillColor(kWhite)
-	leg.AddEntry(dht,"Data")
-	leg.AddEntry(zzht,"ZZ","f")
-	leg.AddEntry(zjht,"Z+X","f")
-
-	#set axis options for data
-	for state in dh:
-		dh[state].GetXaxis().SetTitle(varNice)
-	for hist in [dht, dh['mmmm'], dh['eemm'], dh['eeee']]:
-		if bins[0]-bins[1] == bins[len(bins)-2]-bins[len(bins)-1]: # if spaced evenly
-			div=(float(bins[len(bins)-1])-float(bins[0]))/(len(bins)-1)
-			hist.GetYaxis().SetTitle("Events / %.0f GeV" %div)
-			hist.GetYaxis().SetRange(0,15)
-		else:
-			hist.GetYaxis().SetTitle("Events")
-			hist.GetYaxis().SetRange(0,15)
-	#HACK! efficiencies taken as average of Matt's TP corrected yields/uncorrected
-	eff={}
-	eff["eeee"]=0.979
-	eff["eemm"]=0.9885
-	eff["mmmm"]=0.9876
-	eff["4l"]=(eff["eeee"]+eff["mmmm"]+2.0*eff["eemm"])/4.0
-	f=open(dir+"/yields.txt","w")
-	for state in dh:
-		f.write("---"+state+"---\n")
-		f.write("Data: "+str(dh[state].Integral())+"\n")
-		f.write("ZZ: "+str(zzh[state].Integral()*eff[state])+"\n")
-		f.write("ZJets:"+str(zjh[state].Integral())+"\n")
-		if not os.path.exists(dir+"/"+state):
-			os.makedirs(dir+"/"+state)
-		hs=THStack("hs","stack bg")
-		hs.Add(zjh[state])
-		hs.Add(zzh[state])
-		hs.Add(hh[state])	
-		print state,zzh[state].Integral()
-		ymax=max(dh[state].GetMaximum(),hs.GetMaximum())
-		ymax=ceil(ymax+sqrt(ymax))
-		ymax=int(ymax)
-		l1 = TLatex(bins[0]+2,ymax*1.01,"CMS Preliminary "+year);
-		l1.SetTextSize(0.04);
-		l2 = TLatex(texx,texyf*ymax,"#splitline{L_{int} ="+lumi+" fb^{-1}}{#sqrt{s} = "+l+" TeV}");
-		l2.SetTextSize(0.04);
-		dh[state].GetYaxis().SetRangeUser(0,ymax)
-		dh[state].Draw()
-		hs.Draw("hsame")
-		dh[state].Draw("esame")
-		leg.Draw()
-		l1.Draw();
-		l2.Draw();
-		if bins[0]-bins[1] == bins[len(bins)-2]-bins[len(bins)-1]: # if spaced evenly
-			can.SaveAs(dir+"/"+state+"/"+state+"_"+var+""+postfix+".png")
-			can.SaveAs(dir+"/"+state+"/"+state+"_"+var+""+postfix+".root")
-			can.SaveAs(dir+"/"+state+"/"+state+"_"+var+""+postfix+".C")
-		else:
-			can.SaveAs(dir+"/"+state+"/"+state+"_"+var+"_limitbinning"+postfix+".png")
-			can.SaveAs(dir+"/"+state+"/"+state+"_"+var+"_limitbinning"+postfix+".root")
-			can.SaveAs(dir+"/"+state+"/"+state+"_"+var+"_limitbinning"+postfix+".C")
-	f.close()
+def addDataset(file,color,isMC):
+    temp={}
+    temp["file"]=file
+    temp["color"]=color
+    temp["isMC"]=isMC
+    return temp
 
 if __name__ == '__main__':
-	parser=OptionParser(description="%prog -- dump some analysis-level plots and yields",usage="%prog --extra='extra cuts to apply'")
-	parser.add_option("--extra",dest="extra",type="string",default="1")
-	parser.add_option("--lumi",dest="lumi",type="string",default="5.02")
-	(options,args)=parser.parse_args()
-	extra=options.extra
-	lumi=options.lumi
+    parser=OptionParser(description="%prog -- dump some analysis-level plots and yields",usage="%prog --extra='extra cuts to apply'")
+    parser.add_option("--extra",dest="extra",type="string",default="1")
+    parser.add_option("--lumi",dest="lumi",type="string",default="5.02")
+    (options,args)=parser.parse_args()
+    extra=options.extra
+    lumi=options.lumi
+
+    #todo: define samples, nice names, colors, etc.
+    datasets={}
+    datasets["ZZ"]=addDataset("ZZ4M.root",kAzure-9,True)
+    datasets["ZJets"]=addDataset("DYJets.root",kGreen-5,True)
+    datasets["DATA"]=addDataset("DATA.root",kBlack,False)
+    trees={}
+    trees["mm"]="muMuEventTree/eventTree"
+
+    makePlots(datasets,trees,dir="2012",postfix="8TeV",lumi=lumi,extra=extra,var="mass[0]",varNice="M_{#mu#mu}",bins=range(60,125,1),texx=400,texyf=0.4,legx=0.6)
+    makePlots(datasets,trees,dir="2012",postfix="8TeV",lumi=lumi,extra=extra,var="l1Pt[0]",varNice="#mu_{1} P_{T}",bins=range(0,60,1),texx=400,texyf=0.4,legx=0.6)
+    makePlots(datasets,trees,dir="2012",postfix="8TeV",lumi=lumi,extra=extra,var="l2Pt[0]",varNice="#mu_{2} P_{T}",bins=range(0,60,1),texx=400,texyf=0.4,legx=0.6)
+    makePlots(datasets,trees,dir="2012",postfix="8TeV",lumi=lumi,extra=extra,var="vertices[0]",varNice="Reco. Vertices",bins=range(0,50,1),texx=400,texyf=0.4,legx=0.6)
 
 #	makePlots(dir="2012",postfix="8TeV",lumi=lumi,extra=extra,var="mass",varNice="M_{4l}",bins=range(80,610,10),texx=400,texyf=0.4,legx=0.6)
 #	makePlots(dir="2012",postfix="8TeV",lumi=lumi,extra=extra,var="mass",varNice="M_{4l}",bins=range(90,170,5),texx=400,texyf=0.4,legx=0.6)
@@ -249,9 +247,9 @@ if __name__ == '__main__':
 #	makePlots(dir="2012",postfix="8TeV",lumi=lumi,extra=extra,var="z2Mass",varNice="Z_{M}^{2}",bins=range(60,125,5),texx=63,legx=0.2)
 #	makePlots(dir="2012",postfix="8TeV",lumi=lumi,extra=extra,var="vertices",varNice="Reco. Vertices",bins=range(0,50,50),texx=131,texyf=0.4,legx=0.7)
 #	makeScatter("DATA_12Jun_wMuEG_combed_plusFSR.root","2012",lumi)
-	makeScatter("DATAfinal.root","2011","5.02")
-	f=open("2012/yields.txt")
-	print "****----"+extra+"----"
-	for line in f:
-		print line.rstrip("\n\r")
-	f.close()
+#	makeScatter("DATAfinal.root","2011","5.02")
+#	f=open("2012/yields.txt")
+#	print "****----"+extra+"----"
+#	for line in f:
+#		print line.rstrip("\n\r")
+#	f.close()
