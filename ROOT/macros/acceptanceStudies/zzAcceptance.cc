799720324786f5c63b6623d39dd584e2cf73db00
Austin Belknap <dabelknap@wisc.edu>
2012-07-27 06:53:23 -0500

    Debugged pt cuts

--- ROOT/macros/acceptanceStudies/zzAcceptance.cc
+++ ROOT/macros/acceptanceStudies/zzAcceptance.cc
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
+#include <iomanip>
+#include <algorithm>
 #include "TTree.h"
 #include "TFile.h"
 
+using namespace std;
+
 bool ptCut(int pdgId, double pt);
 void zzAcceptance();
+bool passPtCuts(double pt[], int pdgId[]);
 
 int main()
 {
@@ -16,36 +21,10 @@ int main()
 
 void zzAcceptance()
 {
-    //TFile *f = new TFile("/scratch/belknap/ZZGenLvl.root");
-    TFile *f = new TFile("/afs/hep.wisc.edu/cms/belknap/UWTest/src/UWAnalysis/CRAB/LLLL/analysis.root");
+    TFile *f = new TFile("/scratch/belknap/ZZGenLvl.root");
+    //TFile *f = new TFile("/afs/hep.wisc.edu/cms/belknap/UWTest/src/UWAnalysis/CRAB/LLLL/analysis.root");
     
     TTree *t = (TTree*)f->Get("genlevel/genEventTree");
-
-    // examine only electrons and muons
-    //std::string selection = "(abs(z1l1pdgId) == 11 || abs(z1l1pdgId) == 13) && (abs(z2l1pdgId) == 11 || abs(z2l1pdgId) == 13)";
-    //std::cout << "Starting Events: " << t->GetEntries(selection.c_str()) << std::endl;
-    
-    // apply mass cuts to both Z's
-    //selection += "&& 60 < z1Mass && z1Mass < 120 && 60 < z2Mass && z2Mass < 120";
-    //std::cout << "    Z Mass Cuts: " << t->GetEntries(selection.c_str()) << std::endl;
-
-    // apply eta cuts to daughter leptons
-    //selection += "&& ( ( abs(z1l1pdgId) == 11 && abs(z1l1Eta) < 2.5 && abs(z1l2Eta) < 2.5 ) || ( abs(z2l1pdgId) == 13 && abs(z2l1Eta) < 2.4 && abs(z2l2Eta) < 2.4 ) )";
-    //std::cout << "       Eta Cuts: " << t->GetEntries(selection.c_str()) << std::endl;
-
-    //selection += "&& (";
-    //selection += "( z1l1Pt > 20 && z1l2Pt > 10 && abs(z2l1pdgId) == 11 && z2l1Pt > 7 && z2l2Pt > 7 ) ||";
-    //selection += "( z1l1Pt > 20 && z1l2Pt > 10 && abs(z2l1pdgId) == 13 && z2l1Pt > 5 && z2l2Pt > 5 ) ||";
-
-    //selection += "( z1l1Pt > 20 && abs(z1l2pdgId) == 11 && z1l2Pt > 7 && z2l1Pt > 10 && abs(z2l2pdgId) == 11 && z2l2Pt > 7 ) ||";
-    //selection += "( z1l1Pt > 20 && abs(z1l2pdgId) == 11 && z1l2Pt > 7 && z2l1Pt > 10 && abs(z2l2pdgId) == 13 && z2l2Pt > 5 ) ||";
-    //selection += "( z1l1Pt > 20 && abs(z1l2pdgId) == 13 && z1l2Pt > 5 && z2l1Pt > 10 && abs(z2l2pdgId) == 11 && z2l2Pt > 7 ) ||";
-    //selection += "( z1l1Pt > 20 && abs(z1l2pdgId) == 13 && z1l2Pt > 5 && z2l1Pt > 10 && abs(z2l2pdgId) == 13 && z2l2Pt > 5 ) ||";
-
-    //selection += "( z1l1Pt > 20 && abs(z1l2pdgId) == 11 && z1l2Pt > 7 && z2l1Pt > 10 && abs(z2l2pdgId) == 11 && z2l2Pt > 7 ) ||";
-    //selection += "( z1l1Pt > 20 && abs(z1l2pdgId) == 11 && z1l2Pt > 7 && z2l1Pt > 10 && abs(z2l2pdgId) == 13 && z2l2Pt > 5 ) ||";
-    //selection += "( z1l1Pt > 20 && abs(z1l2pdgId) == 13 && z1l2Pt > 5 && z2l1Pt > 10 && abs(z2l2pdgId) == 11 && z2l2Pt > 7 ) ||";
-    //selection += "( z1l1Pt > 20 && abs(z1l2pdgId) == 13 && z1l2Pt > 5 && z2l1Pt > 10 && abs(z2l2pdgId) == 13 && z2l2Pt > 5 ) ||";
 
     int z1l1pdgId;
     int z1l2pdgId;
@@ -86,9 +65,6 @@ void zzAcceptance()
     int pdgId[4];
     double pt[4];
 
-    // permutations of the indicies for the four leptons
-    int cutId [] = {1234,1243,1324,1342,1423,1432,2134,2143,2314,2341,2413,2431,3124,3142,3214,3241,3412,3421,4123,4132,4213,4231,4312,4321};
-
     int startCounts = 0;
     int massCounts  = 0;
     int etaCounts   = 0;
@@ -124,35 +100,44 @@ void zzAcceptance()
                     pt[2] = z2l1Pt;
                     pt[3] = z2l2Pt;
 
-                    // loop over permutations
-                    for (int j = 0; j < 24; ++j)
-                    {
-                        // extract indicies
-                        int id      = cutId[j];
-                        int first   = id%10; id /= 10;
-                        int second  = id%10; id /= 10;
-                        int third   = id%10; id /= 10;
-                        int fourth  = id%10;
-
-                        // Update counts if pT cuts are passed
-                        if ( pt[first] > 20 && pt[second] > 10 && ptCut(pdgId[third], pt[third]) && ptCut(pdgId[fourth], pt[fourth]) )
-                        {
-                            ptCounts++;
-                            break;
-                        }
-                    }
+                    if ( passPtCuts(pt, pdgId) )
+                        ptCounts++;
                 }
             }
         }
     }
 
-    std::cout << "Starting Events: " << startCounts << std::endl;
-    std::cout << "    Z Mass Cuts: " << massCounts  << std::endl;
-    std::cout << "       Eta Cuts: " << etaCounts   << std::endl;
-    std::cout << "        pT Cuts: " << ptCounts    << std::endl;
+    cout << setw(15) << "Starting Events" << setw(8) << startCounts << " " << double(startCounts)/double(startCounts) << endl;
+    cout << setw(15) << "Z Mass Cuts"     << setw(8) << massCounts  << " " << double(massCounts)/double(startCounts)  << endl;
+    cout << setw(15) << "Eta Cuts"        << setw(8) << etaCounts   << " " << double(etaCounts)/double(startCounts)   << endl;
+    cout << setw(15) << "pT Cuts"         << setw(8) << ptCounts    << " " << double(ptCounts)/double(startCounts)    << endl;
 }
 
-// does lepton pass pT cut?
+/**
+ * Do the leptons pass the pt cuts?
+ */
+bool passPtCuts(double pt[], int pdgId[])
+{
+    int cutId [] = {0,1,2,3};
+
+    do
+    {
+        int l1 = cutId[0];
+        int l2 = cutId[1];
+        int l3 = cutId[2];
+        int l4 = cutId[3];
+
+        if ( pt[l1] > 20 && pt[l2] > 10 && ptCut(pdgId[l3], pt[l3]) && ptCut(pdgId[l4], pt[l4]) )
+            return true;
+    }
+    while ( next_permutation(cutId,cutId+4) );
+
+    return false;
+}
+
+/**
+ * Does the lepton (mu,e) pass trig. threshold?
+ */
 bool ptCut(int pdgId, double pt)
 {
     if ( pdgId == 11 || pdgId == -11 ) // electrons
