be8fd746427ff2ac4b808c9e587b79d9b4465042
Ian Ross <iross@login06.hep.wisc.edu>
2012-09-10 13:46:48 -0500

    Dump analyzed lumis into JSON for data

--- CRAB/LLLL/mergeJobs.py
+++ CRAB/LLLL/mergeJobs.py
@@ -1,5 +1,5 @@
 '''
-File: submitJobs.py
+File: mergeJobs.py
 Author: Ian Ross (iross@cern.ch), University of Wisconsin Madison
 Description: Generate job submission file (for merging analysis root files).
 '''
@@ -26,7 +26,7 @@ def makeFileList(dataset):
     """Dump list of completed files into a .txt file. It's used for merging. And for keeping track of what's done, I guess."""
     subprocess.call("mkdir -p fileLists",shell=True)
     subprocess.call("touch fileLists/{dataset}.txt".format(dataset=dataset),shell=True)
-    subprocess.call("ls /hdfs/store/user/$USER/{dataset}_{tag}-{type}/*/*.root | sed -e 's/\/hdfs//g' > fileLists/{dataset}.txt".format(dataset=dataset,tag=tag,type=datasets[dataset]['type']),shell=True)
+    subprocess.call("ls /hdfs/store/user/$USER/{dataset}_{tag}-{type}_{dataset}/*/*.root | sed -e 's/\/hdfs//g' > fileLists/{dataset}.txt".format(dataset=dataset,tag=tag,type=datasets[dataset]['type']),shell=True)
 
 for dataset in datasets:
     passes = True
@@ -38,6 +38,9 @@ for dataset in datasets:
     if passes:
         makeFileList(dataset)
         merge.write('farmoutAnalysisJobs --skip-existing-output --output-dir=. --merge {dataset}_{tag} $CMSSW_BASE --input-file-list=fileLists/{dataset}.txt --input-files-per-job=300\n'.format(dataset=dataset,tag=tag))
+        if datasets[dataset]['type']=="DATA":
+            merge.write('jobReportSummary /scratch/$USER/{dataset}_{tag}-DATA_{dataset}/*/*.xml --json-out /scratch/$USER/{dataset}_{tag}.json\n'.format(dataset=dataset,tag=tag))
+            
 
 file.close()
 merge.close()
