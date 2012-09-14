f78ea6294f4c7ac409f4432b2c4e133c7342633e
Ian Ross <iross@login06.hep.wisc.edu>
2012-09-14 09:35:53 -0500

    ZH submit scripts update.. I should just throw LLLL and ZH submission into a common place

--- CRAB/ZH/mergeJobs.py
+++ CRAB/ZH/mergeJobs.py
@@ -1,28 +1,24 @@
 '''
-File: submitJobs.py
+File: mergeJobs.py
 Author: Ian Ross (iross@cern.ch), University of Wisconsin Madison
-Description: Generate job submission file.
+Description: Generate job submission file (for merging analysis root files).
 '''
 
 import json
 import fnmatch
 import subprocess
-from optparse import OptionParser
+from RecoLuminosity.LumiDB import argparse
 
-parser=OptionParser(description="%prog -- dump some analysis-level plots and yields",usage="%prog --extra='extra cuts to apply'")
-parser.add_option("--tag",dest="tag",type="string",default="")
-parser.add_option("--json",dest="json",type="string",default="datasets.json")
-parser.add_option("--samples",dest="samples",type="string",default="")
-(options,args)=parser.parse_args()
+parser=argparse.ArgumentParser(description="%prog -- dump some analysis-level plots and yields")
+parser.add_argument("--tag",dest="tag",type=str,default="")
+parser.add_argument("--json",dest="json",type=str,default="datasets.json")
+parser.add_argument("--samples",dest="samples",nargs="+",type=str,default="")
+args=parser.parse_args()
 
-file = open(options.json)
-tag = options.tag
+file = open(args.json)
+tag = args.tag
 
-out = open("submitJobs.sh","write")
 merge = open("mergeJobs.sh","write")
-
-merge.write("mkdir sandbox\n")
-merge.write("cd sandbox\n")
 
 datasets = json.load(file)
 
@@ -30,17 +26,21 @@ def makeFileList(dataset):
     """Dump list of completed files into a .txt file. It's used for merging. And for keeping track of what's done, I guess."""
     subprocess.call("mkdir -p fileLists",shell=True)
     subprocess.call("touch fileLists/{dataset}.txt".format(dataset=dataset),shell=True)
-    subprocess.call("ls /hdfs/store/user/$USER/{dataset}_{tag}-{type}/*/*.root | sed -e 's/\/hdfs//g' > fileLists/{dataset}.txt".format(dataset=dataset,tag=tag,type=datasets[dataset]['type']),shell=True)
+    subprocess.call("ls /hdfs/store/user/$USER/{dataset}_{tag}-{type}_{dataset}/*/*.root | sed -e 's/\/hdfs//g' > fileLists/{dataset}.txt".format(dataset=dataset,tag=tag,type=datasets[dataset]['type']),shell=True)
 
 for dataset in datasets:
     passes = True
-    if options.samples:
+    if args.samples:
         passes = False
-        if fnmatch.fnmatchcase(dataset, options.samples):
-            passes = True
+        for pattern in args.samples:
+            if fnmatch.fnmatchcase(dataset, pattern):
+                passes = True
     if passes:
         makeFileList(dataset)
-        merge.write('farmoutAnalysisJobs --skip-existing-output --output-dir=. --merge {dataset}_{tag} $CMSSW_BASE --input-file-list=../fileLists/{dataset}.txt --input-files-per-job=300\n'.format(dataset=dataset,tag=tag))
+        merge.write('farmoutAnalysisJobs --skip-existing-output --output-dir=. --merge {dataset}_{tag} $CMSSW_BASE --input-file-list=fileLists/{dataset}.txt --input-files-per-job=300\n'.format(dataset=dataset,tag=tag))
+        if datasets[dataset]['type']=="DATA":
+            merge.write('jobReportSummary /scratch/$USER/{dataset}_{tag}-DATA_{dataset}/*/*.xml --json-out /scratch/$USER/{dataset}_{tag}.json\n'.format(dataset=dataset,tag=tag))
+            
 
 file.close()
 merge.close()
