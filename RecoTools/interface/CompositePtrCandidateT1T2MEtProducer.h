3a4ee04bb34f4445776031216051038d59837e7a
Ian Ross <iross@login05.hep.wisc.edu>
2012-11-12 08:58:08 -0600

    whitespace

--- RecoTools/interface/CompositePtrCandidateT1T2MEtProducer.h
+++ RecoTools/interface/CompositePtrCandidateT1T2MEtProducer.h
@@ -3,11 +3,11 @@
 
 /** \class CompositePtrCandidateT1T2MEtProducer
  *
- * Produce combinations of leptonic and hadronic decay products 
- * of a pair of tau leptons plus missing transverse momentum 
- * (representing the undetected momentum carried away by the neutrinos 
- *  produced in the two tau decays) 
- * 
+ * Produce combinations of leptonic and hadronic decay products
+ * of a pair of tau leptons plus missing transverse momentum
+ * (representing the undetected momentum carried away by the neutrinos
+ *  produced in the two tau decays)
+ *
  * \authors Colin Bernet,
  *          Michal Bluj,
  *          Christian Veelken
@@ -47,13 +47,13 @@
 #include <string>
 
 template<typename T1, typename T2>
-class CompositePtrCandidateT1T2MEtProducer : public edm::EDProducer 
+class CompositePtrCandidateT1T2MEtProducer : public edm::EDProducer
 {
   typedef edm::Ptr<T1> T1Ptr;
   typedef edm::Ptr<T2> T2Ptr;
   typedef std::vector<edm::Ptr<pat::Jet> > JetPtrVector;
   typedef std::vector<CompositePtrCandidateT1T2MEt<T1,T2> > CompositePtrCandidateCollection;
-  
+
  public:
 
   explicit CompositePtrCandidateT1T2MEtProducer(const edm::ParameterSet& cfg)
@@ -73,21 +73,21 @@ class CompositePtrCandidateT1T2MEtProducer : public edm::EDProducer
 
     //met calibration
 
-    edm::ParameterSet calibrationSet =cfg.getParameter<edm::ParameterSet>("metCalibration"); 
+    edm::ParameterSet calibrationSet =cfg.getParameter<edm::ParameterSet>("metCalibration");
     calibrator_ = new METCalibrator(calibrationSet);
     algorithm_.setMETCalibrator(calibrator_);
-				    
+
 
 
 //--- check that InputTag for MET collection has been defined,
-//    in case it is needed for the reconstruction mode 
+//    in case it is needed for the reconstruction mode
 //    specified in the configuration parameter set
     if ( srcMET_.label() == "" && recoMode_ != "" ) {
-      edm::LogError ("CompositePtrCandidateT1T2MEtProducer") << " Configuration Parameter srcMET undefined," 
+      edm::LogError ("CompositePtrCandidateT1T2MEtProducer") << " Configuration Parameter srcMET undefined,"
 							     << " needed for recoMode = " << recoMode_ << " !!";
       cfgError_ = 1;
     }
-    
+
     produces<CompositePtrCandidateCollection>("");
   }
 
@@ -98,10 +98,10 @@ class CompositePtrCandidateT1T2MEtProducer : public edm::EDProducer
     //Instantiate the vertex Algorithm
 //    CompositePtrCandidateT1T2MEtVertexAlgorithm<T1,T2> vAlgo(evt,es);
 
-//--- print-out an error message and add an empty collection to the event 
+//--- print-out an error message and add an empty collection to the event
 //    in case of erroneous configuration parameters
     if ( cfgError_ ) {
-      edm::LogError ("produce") << " Error in Configuration ParameterSet" 
+      edm::LogError ("produce") << " Error in Configuration ParameterSet"
 				<< " --> CompositePtrCandidateT1T2MEt collection will NOT be produced !!";
       std::auto_ptr<CompositePtrCandidateCollection> emptyCompositePtrCandidateCollection(new CompositePtrCandidateCollection());
       evt.put(emptyCompositePtrCandidateCollection);
@@ -119,12 +119,12 @@ class CompositePtrCandidateT1T2MEtProducer : public edm::EDProducer
     typedef edm::View<T2> T2View;
     edm::Handle<T2View> leg2Collection;
     pf::fetchCollection(leg2Collection, srcLeg2_, evt);
-  
+
     reco::CandidatePtr metPtr;
     if ( srcMET_.label() != "" ) {
       edm::Handle<reco::CandidateView> metCollection;
       pf::fetchCollection(metCollection, srcMET_, evt);
-      
+
 //--- check that there is exactly one MET object in the event
 //    (missing transverse momentum is an **event level** quantity)
       if ( metCollection->size() == 1 ) {
@@ -136,7 +136,7 @@ class CompositePtrCandidateT1T2MEtProducer : public edm::EDProducer
 	evt.put(emptyCompositePtrCandidateCollection);
 	return;
       }
-    } 
+    }
 
     const reco::GenParticleCollection* genParticles = 0;
     if ( srcGenParticles_.label() != "" ) {
@@ -150,10 +150,10 @@ class CompositePtrCandidateT1T2MEtProducer : public edm::EDProducer
     for(unsigned int i=0;i<jetCollection->size();++i)
       if(jetCollection->at(i).pt()>minPt_)
 	pfJets.push_back(jetCollection->ptrAt(i));
-      
 
 
-//--- check if only one combination of tau decay products 
+
+//--- check if only one combination of tau decay products
 //    (the combination of highest Pt object in leg1 collection + highest Pt object in leg2 collection)
 //    shall be produced, or all possible combinations of leg1 and leg2 objects
     std::auto_ptr<CompositePtrCandidateCollection> compositePtrCandidateCollection(new CompositePtrCandidateCollection());
@@ -162,7 +162,7 @@ class CompositePtrCandidateT1T2MEtProducer : public edm::EDProducer
 //--- find highest Pt particles in leg1 and leg2 collections
       int idxLeadingLeg1 = -1;
       double leg1PtMax = 0.;
-      for ( unsigned idxLeg1 = 0, numLeg1 = leg1Collection->size(); 
+      for ( unsigned idxLeg1 = 0, numLeg1 = leg1Collection->size();
 	    idxLeg1 < numLeg1; ++idxLeg1 ) {
 	T1Ptr leg1Ptr = leg1Collection->ptrAt(idxLeg1);
 	if ( idxLeadingLeg1 == -1 || leg1Ptr->pt() > leg1PtMax ) {
@@ -170,38 +170,38 @@ class CompositePtrCandidateT1T2MEtProducer : public edm::EDProducer
 	leg1PtMax = leg1Ptr->pt();
 	}
       }
-      
+
       int idxLeadingLeg2 = -1;
       double leg2PtMax = 0.;
-      for ( unsigned idxLeg2 = 0, numLeg2 = leg2Collection->size(); 
+      for ( unsigned idxLeg2 = 0, numLeg2 = leg2Collection->size();
 	    idxLeg2 < numLeg2; ++idxLeg2 ) {
 	T2Ptr leg2Ptr = leg2Collection->ptrAt(idxLeg2);
 
-//--- do not create CompositePtrCandidateT1T2MEt object 
+//--- do not create CompositePtrCandidateT1T2MEt object
 //    for combination of particle with itself
 	if ( idxLeadingLeg1 != -1 ) {
 	  T1Ptr leadingLeg1Ptr = leg1Collection->ptrAt(idxLeadingLeg1);
 	  double dR = reco::deltaR(leadingLeg1Ptr->p4(), leg2Ptr->p4());
 	  if ( dR < dRmin12_ ) continue;
 	}
-	
+
 	if ( idxLeadingLeg2 == -1 || leg2Ptr->pt() > leg2PtMax ) {
 	  idxLeadingLeg2 = idxLeg2;
 	  leg2PtMax = leg2Ptr->pt();
 	}
       }
-      
+
       if ( idxLeadingLeg1 != -1 &&
 	   idxLeadingLeg2 != -1 ) {
 	T1Ptr leadingLeg1Ptr = leg1Collection->ptrAt(idxLeadingLeg1);
 	T2Ptr leadingLeg2Ptr = leg2Collection->ptrAt(idxLeadingLeg2);
-	
-	CompositePtrCandidateT1T2MEt<T1,T2> compositePtrCandidate = 
+
+	CompositePtrCandidateT1T2MEt<T1,T2> compositePtrCandidate =
 	  algorithm_.buildCompositePtrCandidate(leadingLeg1Ptr, leadingLeg2Ptr,pfJets, metPtr, genParticles);
 	compositePtrCandidateCollection->push_back(compositePtrCandidate);
       } else {
 	if ( verbosity_ >= 1 ) {
-	  edm::LogInfo ("produce") << " Found no combination of particles in Collections" 
+	  edm::LogInfo ("produce") << " Found no combination of particles in Collections"
 				   << " leg1 = " << srcLeg1_ << " and leg2 = " << srcLeg2_ << ".";
 	}
       }
@@ -210,22 +210,22 @@ class CompositePtrCandidateT1T2MEtProducer : public edm::EDProducer
 //    if so, skip diTau(j,i), j > i combination in order to avoid two diTau objects being produced
 //    for combinations (i,j) and (j,i) of the same pair of particles in leg1 and leg2 collections
       bool sameCollection = (leg1Collection.id () == leg2Collection.id());
-   
-      for ( unsigned idxLeg1 = 0, numLeg1 = leg1Collection->size(); 
+
+      for ( unsigned idxLeg1 = 0, numLeg1 = leg1Collection->size();
 	    idxLeg1 < numLeg1; ++idxLeg1 ) {
 	T1Ptr leg1Ptr = leg1Collection->ptrAt(idxLeg1);
-	
+
 	unsigned idxLeg2_first = ( sameCollection ) ? (idxLeg1 + 1) : 0;
-	for ( unsigned idxLeg2 = idxLeg2_first, numLeg2 = leg2Collection->size(); 
+	for ( unsigned idxLeg2 = idxLeg2_first, numLeg2 = leg2Collection->size();
 	      idxLeg2 < numLeg2; ++idxLeg2 ) {
 	  T2Ptr leg2Ptr = leg2Collection->ptrAt(idxLeg2);
 
-//--- do not create CompositePtrCandidateT1T2MEt object 
+//--- do not create CompositePtrCandidateT1T2MEt object
 //    for combination of particle with itself
 	  double dR = reco::deltaR(leg1Ptr->p4(), leg2Ptr->p4());
 	  if ( dR < dRmin12_ ) continue;
-	  
-	  CompositePtrCandidateT1T2MEt<T1,T2> compositePtrCandidate = 
+
+	  CompositePtrCandidateT1T2MEt<T1,T2> compositePtrCandidate =
 	    algorithm_.buildCompositePtrCandidate(leg1Ptr, leg2Ptr, pfJets,metPtr, genParticles);
 
 //	  vAlgo.calculateVertexVariables(compositePtrCandidate);
@@ -241,7 +241,7 @@ class CompositePtrCandidateT1T2MEtProducer : public edm::EDProducer
  private:
 
   CompositePtrCandidateT1T2MEtAlgorithm<T1,T2> algorithm_;
-  
+
   bool useLeadingTausOnly_;
   edm::InputTag srcLeg1_;
   edm::InputTag srcLeg2_;
