1649f06bd8439aafd5d853c0dc5d2c54713673ed
Ian Ross <iross@login05.hep.wisc.edu>
2012-11-16 04:49:23 -0600

    rm MCTools

--- MCTools/python/ZLLPhaseSpaceAnalyzer_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-mcAnalysis = cms.EDAnalyzer('ZLLPhaseSpaceAnalyzer',
-                            motherBosonID = cms.int32(23),
-                            leptonID = cms.int32(13),
-                            FileName = cms.string('mcOutput.root')
-)                            
-
