590cd37f557dfeae770a6b4d1604bfc97708daac
Ian Ross <iross@login05.hep.wisc.edu>
2013-01-23 13:38:17 -0600

    rm cout

--- RecoTools/plugins/PATMuonRochesterEmbedder.cc
+++ RecoTools/plugins/PATMuonRochesterEmbedder.cc
@@ -43,13 +43,11 @@ void PATMuonRochesterEmbedder::produce(edm::Event& evt, const edm::EventSetup& e
 
     for (size_t i = 0; i < muons->size(); ++i) {
         pat::Muon muon(muons->at(i));
-        std::cout << "before: " << muon.pt() << std::endl;
         TLorentzVector p4(muon.px(),muon.py(),muon.pz(),muon.energy());
         if (isMC_) corrector2012_.momcor_mc(p4, muon.charge(), 0.0, 0, isSync_);
         else corrector2012_.momcor_data(p4, muon.charge(), 0.0, 0);
         math::XYZTLorentzVector corr_p4(p4.Px(),p4.Py(),p4.Pz(),p4.Energy());
         muon.setP4(corr_p4);
-        std::cout << "after: " << muon.pt() << std::endl;
         out->push_back(muon);
     }
     evt.put(out);
