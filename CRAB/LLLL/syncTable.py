84c148509027eb18aabf326cd76b56e513fd4c0c
Austin Belknap <dabelknap@wisc.edu>
2013-01-23 09:29:58 -0600

    Prints event info

--- CRAB/LLLL/syncTable.py
+++ CRAB/LLLL/syncTable.py
@@ -14,13 +14,60 @@ mmmm_tree = input_file.Get("muMuMuMuEventTreeFinal/eventTree")
 mmee_tree = input_file.Get("muMuEleEleEventTreeFinal/eventTree")
 eeee_tree = input_file.Get("eleEleEleEleEventTreeFinal/eventTree")
 
-selections = "mass > 70 && subBestZmass > 12"
+#selections = "mass > 70 && subBestZmass > 12"
 #selections = "mass > 70 && subBestZmass > 12 && mass > 100 && kd > 0.1"
 
-#selections = "mass > 70 && subBestZmass > 12 && mass > 100 && VBFjets == 2"
-selections = "mass > 70 && subBestZmass > 12 && mass > 100 && VBFjets == 2 && VBFjets_fisher > 0.4"
+events1 = set()
+events2 = set()
 
-print "All    4e   4mu 2e2mu"
+selections = "subBestZmass > 12 && mass > 100 && VBFjets == 2"
+
+mmmm_sel1 = mmmm_tree.CopyTree(selections)
+mmee_sel1 = mmee_tree.CopyTree(selections)
+eeee_sel1 = eeee_tree.CopyTree(selections)
+
+for evt in mmmm_sel1:
+    events1.add( evt.EVENT )
+
+for evt in mmee_sel1:
+    events1.add( evt.EVENT )
+
+for evt in eeee_sel1:
+    events1.add( evt.EVENT )
+
+selections = "subBestZmass > 12 && mass > 100 && VBFjets == 2 && VBFjets_fisher > 0.4"
+
+mmmm_sel2 = mmmm_tree.CopyTree(selections)
+mmee_sel2 = mmee_tree.CopyTree(selections)
+eeee_sel2 = eeee_tree.CopyTree(selections)
+
+for evt in mmmm_sel2:
+    events2.add( evt.EVENT )
+
+for evt in mmee_sel2:
+    events2.add( evt.EVENT )
+
+for evt in eeee_sel2:
+    events2.add( evt.EVENT )
+
+missing_events = events1 - events2
+
+print '{0:>10} {1:>10} {2:>10} {3:>10} {4:>10} {5:>10} {6:>10} {7:>10} {8:>10} {9:>10} {10:>10} {11:>10}'.format("Event","mass","z1mass","z2mass","z1l1Pt","z1l2Pt","z2l1Pt","z2l2Pt","jet1pt","jet2pt","nJets","fisher")
+
+for evt in mmmm_tree:
+    if evt.EVENT in missing_events:
+        print '{0:10} {1:10f} {2:10f} {3:10f} {4:10f} {5:10f} {6:10f} {7:10f} {8:10f} {9:10f} {10:10f} {11:10f}'.format( 
+                evt.EVENT, evt.mass, evt.z1Mass, evt.z2Mass, evt.z1l1Pt, evt.z1l2Pt, evt.z2l1Pt, evt.z2l2Pt, evt.VBFjets_jet1pt, evt.VBFjets_jet2pt, evt.VBFjets, evt.VBFjets_fisher)
+
+for evt in mmee_tree:
+    if evt.EVENT in missing_events:
+        print '{0:10} {1:10f} {2:10f} {3:10f} {4:10f} {5:10f} {6:10f} {7:10f} {8:10f} {9:10f} {10:10f} {11:10f}'.format( 
+                evt.EVENT, evt.mass, evt.z1Mass, evt.z2Mass, evt.z1l1Pt, evt.z1l2Pt, evt.z2l1Pt, evt.z2l2Pt, evt.VBFjets_jet1pt, evt.VBFjets_jet2pt, evt.VBFjets, evt.VBFjets_fisher)
+
+for evt in eeee_tree:
+    if evt.EVENT in missing_events:
+        print '{0:10} {1:10f} {2:10f} {3:10f} {4:10f} {5:10f} {6:10f} {7:10f} {8:10f} {9:10f} {10:10f} {11:10f}'.format( 
+                evt.EVENT, evt.mass, evt.z1Mass, evt.z2Mass, evt.z1l1Pt, evt.z1l2Pt, evt.z2l1Pt, evt.z2l2Pt, evt.VBFjets_jet1pt, evt.VBFjets_jet2pt, evt.VBFjets, evt.VBFjets_fisher)
 
 mmmm_events = mmmm_tree.GetEntries(selections)
 mmee_events = mmee_tree.GetEntries(selections)
@@ -28,4 +75,5 @@ eeee_events = eeee_tree.GetEntries(selections)
 
 total = mmmm_events + mmee_events + eeee_events
 
-print '{0:3d} {1:5d} {2:5d} {3:5d}'.format(total,eeee_events,mmmm_events,mmee_events)
+print '{0:3} {1:>4} {2:>4} {3:>5}'.format("All","4e","4mu","2mu2e")
+print '{0:3d} {1:4d} {2:4d} {3:5d}'.format(total,eeee_events,mmmm_events,mmee_events)
